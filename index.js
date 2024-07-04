00f4531e7081b6abc839bdf837342835142ef3f3
MalikL2005 <163858025+MalikL2005@users.noreply.github.com>
2024-07-04 17:44:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,26 @@ app.get("/", function (req, res) {
 });
 
 
+app.get('/api/:date?', (req, res)=>{
+  //empty
+  if (typeof req.params.date === 'undefined') res.json({unix: Date.now(), utc: new Date(Date.now()).toUTCString()});
+  //date-format
+  else if (req.params.date.match("[0-9]{4}-[0-9]{2}-[0-9]{2}")|| req.params.date.includes(" ")){
+    var d = new Date(req.params.date);
+    res.json({unix: d.getTime(), utc: d.toUTCString()});
+  } 
+  //unix-format
+  else {
+    try {
+      var unix_num = Number(req.params.date);
+      if (new Date(unix_num) == "Invalid Date") throw new Error('invalid input');
+      res.json({unix: unix_num, utc: new Date(unix_num).toUTCString()});
+    } 
+    //invalid
+    catch (err) { res.json({ error : "Invalid Date" });}
+  }
+  });
+
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
